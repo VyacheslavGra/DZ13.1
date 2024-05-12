082e18c641fdfec86c95544559c3e8c1b2196500
Slavik <sapelkin.00@yandex.ru>
2024-05-12 18:22:07 +0300

    2

--- src/item.py
+++ src/item.py
@@ -1,4 +1,16 @@
 import csv
+import os
+
+
+class InstantiateCSVError(Exception):
+    """
+    Класс исключения при повреждении файла
+    """
+    def __init__(self, *args, **kwargs):
+        self.message = args[0] if args else "Файл item.csv поврежден"
+
+    def __str__(self):
+        return self.message
 
 
 class Item:
@@ -8,7 +20,7 @@ class Item:
     pay_rate = 1.0
     all = []
 
-    def __init__(self, name, price: float, quantity: int) -> None:
+    def __init__(self, name: str, price: float, quantity: int) -> None:
         """
         Создание экземпляра класса item.
 
@@ -16,11 +28,24 @@ class Item:
         :param price: Цена за единицу товара.
         :param quantity: Количество товара в магазине.
         """
-        super().__init__()
+
         self.__name = name
         self.price = price
         self.quantity = quantity
-        Item.all.append(self)
+        self.all.append(self)
+
+    @property
+    def name(self):
+        return f"{self.__name}"
+
+    @name.setter
+    def name(self, value):
+        if len(value) <= 10:
+            self.__name = value
+            print(f'Корректное название - {value}')
+        else:
+            self.__name = value[:10]
+            print(f'Длинное слово - {value[:10]}')
 
     def calculate_total_price(self) -> float:
         """
@@ -28,10 +53,33 @@ class Item:
 
         :return: Общая стоимость товара.
         """
-        return self.quantity * self.price
+        all_price = self.quantity * self.price
+        return all_price
 
     def apply_discount(self) -> None:
         """
         Применяет установленную скидку для конкретного товара.
         """
-        self.price *= self.pay_rate
+        self.price = self.price - self.price * self.pay_rate
+
+    @name.setter
+    def name(self, value):
+        self.__name = value
+
+    @classmethod
+    def instantiate_from_csv(cls):
+        cls.all.clear()
+        try:
+            with (open("../src/items.csv", "rt", newline="", encoding="cp1251") as csv_file):
+
+                reader = csv.DictReader(csv_file)
+
+                for object in reader:
+                    if "name" not in object or "price" not in object or "quantity" not in object:
+                        raise InstantiateCSVError
+                    cls(object["name"], object["price"], object["quantity"])
+        except FileNotFoundError:
+            print("FileNotFoundError: Отсутствует файл item.csv")
+        except InstantiateCSVError:
+            print("InstantiateCSVError: Файл item.csv поврежден")
+
--- tests/test_item.py
+++ tests/test_item.py
@@ -4,12 +4,26 @@ from src.item import Item
 
 
 def test_calculate_total_price():
-    item1 = Item("Смартфон", 10000, 20)
-    assert item1.calculate_total_price() == 200000
+    item = Item('Смартфон', 20000, 100)
+    assert item.calculate_total_price() == 2000000
 
 
 def test_apply_discount():
-    item1 = Item("Смартфон", 10000, 20)
-    Item.pay_rate = 0.8
-    item1.apply_discount()
-    assert item1.price == 8000.0
+    item = Item('Смартфон', 2000, 3)
+    Item.pay_rate = 0.1
+
+    item.apply_discount()
+
+    assert item.price == 1800.0
+
+
+def test_name_setter():
+    item = Item('Телефон', 10000, 5)
+    item.name = 'Смартфон'
+    assert item.name == 'Смартфон'
+
+
+def test_name_setter_truncate():
+    item = Item('Телефон', 25000, 3)
+    item.name = 'Суперсмартфон'
+    assert item.name == 'Суперсмартфон'
